5bf7f13a3d8886c11f323ec36a08d32770534c40
Simantick <maxim.marchenko.2017@gmail.com>
2023-03-05 19:55:52 +0200

    Castomized

--- SpotifyUpdater/SpotifyUpdater.cpp
+++ SpotifyUpdater/SpotifyUpdater.cpp
@@ -1,21 +1,33 @@
 ﻿#include <iostream>
 #include <Windows.h>
+#include <Lmcons.h>
 #include <fstream>
 
 using namespace std;
 
-int main(){	
+class Spicetify {
+public:
+	void SetP() {
+		PowCom = "@echo off\n";
+		PowCom += "echo Installing Spicetify...\n";
+		PowCom += "PowerShell -Command \"Start-Sleep -Seconds 5; iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex\"\n";
+		PowCom += "PowerShell -Command \"Start-Sleep -Seconds 5; iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex\"\n";
+		PowCom += "echo Installation complete!\n";
+		PowCom += "pause\n";
+	}
+	string GetPC() {
+		return PowCom;
+	}
+private:
 	string PowCom;
+};
+
+int main(){
+	Spicetify Sp;
 	ofstream file;
-	WinExec("C:\\Users\\maxim\\AppData\\Roaming\\Spotify\\Spotify.exe", 1);
 	file.open("Updater.bat");
-	PowCom = "@echo off\n";
-	PowCom += "echo Installing Spicetify...\n";
-	PowCom += "PowerShell -Command \"Start-Sleep -Seconds 5; iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex\"\n";
-	PowCom += "PowerShell -Command \"Start-Sleep -Seconds 5; iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex\"\n";
-	PowCom += "echo Installation complete!\n";
-	PowCom += "pause\n";
-	file << PowCom << endl;
+	Sp.SetP();
+	file << Sp.GetPC()<< endl;
 	file.close();
 	system("Updater.bat");
 	remove("Updater.bat");
