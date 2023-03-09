886154e2a9de00cbc49bde531f5a480f05aba53d
Simantick <maxim.marchenko.2017@gmail.com>
2023-03-09 22:59:12 +0200

    Complete V1

--- SpotifyUpdater/SpotifyUpdater.cpp
+++ SpotifyUpdater/SpotifyUpdater.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <Lmcons.h>
 #include <fstream>
+#pragma warning(disable: 4996)
 
 using namespace std;
 
@@ -12,14 +13,23 @@ public:
 		PowCom += "echo Installing Spicetify...\n";
 		PowCom += "PowerShell -Command \"Start-Sleep -Seconds 5; iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex\"\n";
 		PowCom += "PowerShell -Command \"Start-Sleep -Seconds 5; iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex\"\n";
-		PowCom += "echo Installation complete!\n";
-		PowCom += "pause\n";
 	}
 	string GetPC() {
 		return PowCom;
 	}
+	void StartSpotify() {
+		appdata = getenv("APPDATA");
+		command1 = appdata;
+		command1 += "\\Spotify\\spotify.exe";
+		wString = new wchar_t[4096];
+		MultiByteToWideChar(CP_ACP, 0, command1.c_str(), -1, wString ,4096);
+		cout << wString << endl;
+		ShellExecute(NULL, L"open", wString, L"", L"C:\\", SW_NORMAL);
+	}
 private:
-	string PowCom;
+	string PowCom,command1, command2;
+	char* appdata;
+	wchar_t* wString;
 };
 
 int main(){
@@ -29,6 +39,7 @@ int main(){
 	Sp.SetP();
 	file << Sp.GetPC()<< endl;
 	file.close();
+	Sp.StartSpotify();
 	system("Updater.bat");
 	remove("Updater.bat");
 }
